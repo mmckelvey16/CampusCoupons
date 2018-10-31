a7161f9236e94029f817cb26d3e1ece08a522371
Matthew <m.mckelvey16@gmail.com>
2018-10-31 15:07:16 -0400

    Initializing repository

---
+++ app/src/main/java/com/example/mmcke/campuscoupons/controllers/OpeningActivity.java
@@ -0,0 +1,38 @@
+package com.example.mmcke.campuscoupons.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+import com.example.mmcke.campuscoupons.R;
+
+public class OpeningActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening);
+
+        //Login button to go to login page
+        Button LoginButton = findViewById(R.id.loginButton);
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button RegisterButton = findViewById(R.id.registerButton);
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
